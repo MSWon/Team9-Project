bb4ee3262ecbacb89266885312459e6aa7134377
minsub <minsubwon@naver.com>
2017-05-07 23:37:54 +0900

    2017/05/07

--- Team9Project/src/OOPEducationProject.java
+++ Team9Project/src/OOPEducationProject.java
@@ -1,5 +1,5 @@
 
-public class test {
+abstract public class OOPEducationProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
---
+++ Team9Project/src/Quiz.java
@@ -0,0 +1,27 @@
+
+public class Quiz {
+	private int ChapterNum;
+	private String[] problem;
+	
+	public int getChapterNum() {
+		return ChapterNum;
+	}
+	public void setChapterNum(int chapterNum) {
+		ChapterNum = chapterNum;
+	}
+	public String[] getProblem() {
+		return problem;
+	}
+	public void setProblem(String[] problem) {
+		this.problem = problem;
+	}
+	
+	public void SetExam(int ChapterNum){
+		
+	}
+	
+	public void GetSolution(String[] problem){
+		
+	}
+	
+}
