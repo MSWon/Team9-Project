f4841944fc58b9a3c92777c20170f04fa435e891
Juhee <mjh0208@ajou.ac.kr>
2017-05-26 15:46:24 +0900

    quiz 초안 1

--- Team9Project/src/Chapter1.java
+++ Team9Project/src/Chapter1.java
@@ -2,6 +2,8 @@ import java.awt.BorderLayout;
 
 public class Chapter1 extends Chapter{
 	
+	private QuizPanel quiz;
+	
 	@Override
 	
 	public void go2(){
@@ -10,10 +12,12 @@ public class Chapter1 extends Chapter{
 		p1 = new TextInPanel();
 		p2 = new Panel2();
 		p3 = new Panel3();
+		quiz = new QuizPanel();
 		
 		Cardpanel.add(p1);
 		Cardpanel.add(p2);
 		Cardpanel.add(p3);
+		Cardpanel.add(quiz);
 		
 		innerpanel.add(Cardpanel);
 		innerpanel.add(Btnpanel);
