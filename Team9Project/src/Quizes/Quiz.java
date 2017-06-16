5d01bdc0a820ccdb0beaa57ac222419601056ccf
Juhee <mjh0208@ajou.ac.kr>
2017-06-16 19:32:04 +0900

    abstract class

--- Team9Project/src/Quizes/Quiz.java
+++ Team9Project/src/Quizes/Quiz.java
@@ -6,7 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
-public class Quiz extends JPanel implements ActionListener{
+public abstract class Quiz extends JPanel implements ActionListener{
 	
 	public Quiz(){
 		this.setBackground(Color.white);
